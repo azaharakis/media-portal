829a30b3ddc9768f5ff6a9e68e711992b29cfbc5
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2016-10-18 22:31:45 +1100

    App and fonts

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './fonts/font-awesome.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      src: ':8080'
+    };
+
+    this.updateIframe = this.updateIframe.bind(this);
+  }
+
+  updateIframe(e){
+    this.setState({
+      src: e
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div id="app">
+          <iframe width="100%" height="100%" src={`http://192.168.1.249${this.state.src}`}></iframe>
+          <div className="menu">
+            <a onClick={ () => this.updateIframe(':8082')}  className="menu__item ">
+              <i className="fa fa-film" aria-hidden="true"></i>
+            </a>
+            <a onClick={ () => this.updateIframe(':8081')}  className="menu__item ">
+              <i  className="fa fa-television" aria-hidden="true"></i>
+            </a>
+          </div>
+          <div className="menu menu--left">
+            <a onClick={ () => this.updateIframe(':8080')}  className="menu__item ">
+              <i className="fa fa-cloud-download" aria-hidden="true"></i>
+            </a>
+            <a onClick={ () => this.updateIframe(':8112')}  className="menu__item ">
+              <i className="fa fa-download" aria-hidden="true"></i>
+            </a>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
